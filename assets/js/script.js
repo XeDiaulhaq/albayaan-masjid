6648f6e5373e20ae57892b2b5b4d87f8882f7435
Cartaphilus <49419636+DhiaUlhaq0@users.noreply.github.com>
2024-07-07 00:31:30 +0700

    e

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 function getJadwalSholat() {
-    fetch('http://api.aladhan.com/v1/timingsByCity?city=Lhokseumawe&country=Indonesia&method=2')
+    fetch('https://api.aladhan.com/v1/timingsByCity?city=Lhokseumawe&country=Indonesia&method=2')
     .then(response => response.json())
     .then(data => {
         const jadwalSholat = data.data.timings;
 
         // Mengisi nilai jadwal sholat ke dalam elemen HTML yang sesuai
-        document.querySelector('.card-title').innerHTML = `<i class="bi bi-geo-alt-fill m-2"></i>Lhokseumawe - ${data.data.date.gregorian.date}`;
+        document.querySelector('.card-title').innerHTML = `<i class="bi bi-geo-alt-fill m-2"></i>Lhokseumawe, Aceh - ${data.data.date.gregorian.date}`;
         document.querySelector('.Imsak span').textContent = jadwalSholat.Imsak;
         document.querySelector('.Subuh span').textContent = jadwalSholat.Fajr;
         document.querySelector('.Dhuzur span').textContent = jadwalSholat.Dhuhr;
